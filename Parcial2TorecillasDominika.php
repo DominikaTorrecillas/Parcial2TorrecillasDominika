3293fed99b36d40b9236e8a8aa2f3613fc9c1860
Domy <dominikatorrecillas@gmail.com>
2024-10-22 12:04:11 -0600

    Add files via upload

---
+++ Parcial2TorecillasDominika.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title></title>
+
+</head>
+
+<body>
+
+<?php
+
+$array= array("huawei"=>"5000", "iphone"=>"9000", "xiaomi"=>"7000", "oppo"=>"6000", "poco"=>"4000");
+
+array_push($array,"nextel"=>"3000"=>"motorola"=>"3500");
+
+foreach($array as $telefonos){
+   echo $telefono. "<br>";  
+}
+
+asort($telefonos); 
+
+?>
+
+</body>
+</html>
